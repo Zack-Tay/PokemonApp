70ceb14d93f72fb7df28f2bd2749585c7adf28b1
Zack-Tay <101039486+Zack-Tay@users.noreply.github.com>
2022-03-14 16:11:24 +0800

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,12 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v2a4t5_5s!5la@70=%!y8c&i9z4!4-coj_xf4fa5-ngqd8i6ia'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
